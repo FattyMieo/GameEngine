9c9f2db6fde9046a8c4de2156be51b58561d971c
MSIROS <msiros@kdu-online.com>
2018-06-26 17:26:24 +0800

    Create UI for BitData

--- GameEngine/BitData_Demo.cpp
+++ GameEngine/BitData_Demo.cpp
@@ -1,57 +1,136 @@
 #include <iostream>
+#include <conio.h>
 #include "BitData.h"
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define TOTAL_BITS sizeof(int) * 8
 
 using namespace std;
 
-void ListContent(BitData data)
+enum KEYSTATE
 {
-	for (int i = 0; i < sizeof(int) * 8; i++)
+	KEYSTATE_INVALID = 0,
+	KEYSTATE_UP,
+	KEYSTATE_DOWN,
+	KEYSTATE_SPACE,
+	KEYSTATE_ENTER,
+	KEYSTATE_0,
+	KEYSTATE_1,
+};
+
+KEYSTATE DetectKey()
+{
+	char c = _getch();
+
+	if (c == (char)0 || c == (char)224) // if the first value is special key
 	{
-		cout << i << ". ";
-		if (data.Get(i))
+		switch ((int)_getch())
 		{
-			cout << "TRUE" << endl;
-		}
-		else
-		{
-			cout << "FALSE" << endl;
+		case KEY_UP:
+			// code for arrow up
+			return KEYSTATE_UP;
+			break;
+		case KEY_DOWN:
+			// code for arrow down
+			return KEYSTATE_DOWN;
+			break;
 		}
 	}
+	else if (c == ' ')
+	{
+		return KEYSTATE_SPACE;
+	}
+	else if (c == '\r')
+	{
+		return KEYSTATE_ENTER;
+	}
+	else if (c == '0')
+	{
+		return KEYSTATE_0;
+	}
+	else if (c == '1')
+	{
+		return KEYSTATE_1;
+	}
+
+	return KEYSTATE_INVALID;
+}
+
+void ListContent(BitData data, int curPos)
+{
+	cout << endl;
+
+	cout << "    Bit Data" << endl;
+
+	cout << endl;
+
+	cout << "     ";
+	for (int i = 0; i < 6; i++)
+		cout << (char)177;
+	cout << endl;
+
+	for (int i = 0; i < TOTAL_BITS; i++)
+	{
+		cout << "     ";
+		for (int j = 0; j < 2; j++)
+			cout << (data.Get(i) ? (char)178 : (char)176);
+
+		cout << (i < 10 ? "0" : "") << i;
+
+		for (int j = 0; j < 2; j++)
+			cout << (data.Get(i) ? (char)178 : (char)176);
+
+		if (i == curPos)
+			cout << " " << (char)254;
+
+		cout << endl;
+	}
+
+	cout << "     ";
+	for (int i = 0; i < 6; i++)
+		cout << (char)177;
+	cout << endl;
+
+	cout << endl;
 }
 
 int main(void)
 {
 	BitData bitData;
+	int curPos = 0;
 
-	bitData.Set(0, true);
-	bitData.Set(4, true);
-	bitData.Set(7, true);
-	bitData.Set(8, true);
+	while (true)
+	{
+		system("CLS");
+		ListContent(bitData, curPos);
 
-	ListContent(bitData);
-
-	system("PAUSE");
-	system("CLS");
-
-	bitData.Set(0, false);
-	bitData.Set(2, true);
-	bitData.Set(4, false);
-	bitData.Set(11, true);
-
-	ListContent(bitData);
-
-	system("PAUSE");
-	system("CLS");
-
-	bitData.Set(0, true);
-	bitData.Set(1, false);
-	bitData.Set(2, true);
-	bitData.Set(3, true);
-	bitData.Set(3, false);
-
-	ListContent(bitData);
-
-	system("PAUSE");
+		switch (DetectKey())
+		{
+			case KEYSTATE_UP:
+				curPos--;
+				if (curPos < 0)
+					curPos = 0;
+				break;
+			case KEYSTATE_DOWN:
+				curPos++;
+				if (curPos >= TOTAL_BITS)
+					curPos = TOTAL_BITS - 1;
+				break;
+			case KEYSTATE_SPACE:
+				bitData.Set(curPos, !bitData.Get(curPos));
+				break;
+			case KEYSTATE_0:
+				bitData.Set(curPos, false);
+				break;
+			case KEYSTATE_1:
+				bitData.Set(curPos, true);
+				break;
+			case KEYSTATE_ENTER:
+				bitData.Reset();
+				break;
+		}
+	}
 
 	return 0;
 }
